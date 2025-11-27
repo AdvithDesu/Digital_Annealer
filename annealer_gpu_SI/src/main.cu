74a61c22707633bb35fafec95d81949c67f57f9d
AdvithDesu <75376191+AdvithDesu@users.noreply.github.com>
2025-11-27 11:59:41 +0530

    Update main.cu
    Adding 0.5 to total energy where needed to avoid double counting

--- annealer_gpu_SI/src/main.cu
+++ annealer_gpu_SI/src/main.cu
@@ -720,7 +720,7 @@ __global__ void init_spins_total_energy(float* gpuAdjMat, unsigned int* gpuAdjMa
 	{
  		// Original vertice_energy
 		// float vertice_energy = ((float)gpuSpins[vertice_Id]) * ( sh_mem_spins_Energy[0] - gpuLinTermsVect[vertice_Id] );
-		float vertice_energy = ((float)gpuSpins[vertice_Id]) * ( sh_mem_spins_Energy[0] + gpuLinTermsVect[vertice_Id] );
+		float vertice_energy = (0.5f) * ((float)gpuSpins[vertice_Id]) * ( sh_mem_spins_Energy[0] + gpuLinTermsVect[vertice_Id] );
 		// hamiltonian_per_spin[vertice_Id] = vertice_energy;// each threadblock updates its own memory location
 
 //		printf("vertice_energy  %f \n", vertice_energy);
@@ -774,7 +774,7 @@ __global__ void final_spins_total_energy(float* gpuAdjMat, unsigned int* gpuAdjM
 	{
         // Original vertice energy
 		// float vertice_energy = ((float)gpuSpins[vertice_Id]) * ( sh_mem_spins_Energy[0] - gpuLinTermsVect[vertice_Id] );
-		float vertice_energy = ((float)gpuSpins[vertice_Id]) * ( sh_mem_spins_Energy[0] + gpuLinTermsVect[vertice_Id] );
+		float vertice_energy = (0.5f) * ((float)gpuSpins[vertice_Id]) * ( sh_mem_spins_Energy[0] + gpuLinTermsVect[vertice_Id] );
 		// hamiltonian_per_spin[vertice_Id] = vertice_energy;// each threadblock updates its own memory location
 
 		//printf("vertice_energy  %d %f \n",vertice_Id, vertice_energy);
@@ -828,8 +828,8 @@ __global__ void preprocess_max_cut_from_ising(float* gpuAdjMat, unsigned int* gp
 
 		float vertice_energy;
 		// Origial vertice_energy
-		// vertice_energy = (0.5f) * sh_mem_spins_Energy[0];
-		vertice_energy = sh_mem_spins_Energy[0];
+		vertice_energy = (0.5f) * sh_mem_spins_Energy[0];
+		// vertice_energy = sh_mem_spins_Energy[0];
 
 		atomicAdd(max_cut_value, vertice_energy);
 
